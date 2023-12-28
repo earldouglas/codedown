af63018539a9c37f228041f12be9d11c4028aad2
James Earl Douglas <james@earldouglas.com>
2023-12-28 13:20:33 -0700

    Add support for subsetting by section heading

--- test/codedown.js
+++ test/codedown.js
@@ -80,7 +80,7 @@ describe('codedown', function(){
     });
   });
 
-  it('should extract code by section (1)', function (done) {
+  it('should extract code by section number (1)', function (done) {
     process.exec('cat README.md | ./codedown.js haskell --section 1', function (err, stdout, stderr) {
       if (!err) {
         assert.equal(
@@ -100,7 +100,7 @@ describe('codedown', function(){
     });
   });
 
-  it('should extract code by section (1.3)', function (done) {
+  it('should extract code by section number (1.3)', function (done) {
     process.exec('cat README.md | ./codedown.js haskell --section 1.3', function (err, stdout, stderr) {
       if (!err) {
         assert.equal(
@@ -120,7 +120,7 @@ describe('codedown', function(){
     });
   });
 
-  it('should extract code by section (1.3.1)', function (done) {
+  it('should extract code by section number (1.3.1)', function (done) {
     process.exec('cat README.md | ./codedown.js haskell --section 1.3.1', function (err, stdout, stderr) {
       if (!err) {
         assert.equal(
@@ -137,8 +137,62 @@ describe('codedown', function(){
     });
   });
 
-  it('should extract code by section (1.3.2)', function (done) {
+  it('should extract code by section number (1.3.2)', function (done) {
     process.exec('cat README.md | ./codedown.js haskell --section 1.3.2', function (err, stdout, stderr) {
+      if (!err) {
+        assert.equal(
+          stdout,
+          [ 'main :: IO ()'
+          , 'main = putStrLn $ show x'
+          , ''
+          ].join('\n')
+        );
+        done();
+      } else {
+        console.log(stderr);
+      }
+    });
+  });
+
+  it('should extract code by section name (## Examples)', function (done) {
+    process.exec('cat README.md | ./codedown.js haskell --section "## Examples"', function (err, stdout, stderr) {
+      if (!err) {
+        assert.equal(
+          stdout,
+          [ 'x :: Int'
+          , 'x = 42'
+          , ''
+          , 'main :: IO ()'
+          , 'main = putStrLn $ show x'
+          , ''
+          ].join('\n')
+        );
+        done();
+      } else {
+        console.log(stderr);
+      }
+    });
+  });
+
+  it('should extract code by section name (### Variables in different languages)', function (done) {
+    process.exec('cat README.md | ./codedown.js haskell --section "### Variables in different languages"', function (err, stdout, stderr) {
+      if (!err) {
+        assert.equal(
+          stdout,
+          [ 'x :: Int'
+          , 'x = 42'
+          , ''
+          ].join('\n')
+        );
+        done();
+      } else {
+        console.log(stderr);
+      }
+    });
+  });
+
+  it('should extract code by section name (### Console output in different languages)', function (done) {
+    process.exec('cat README.md | ./codedown.js haskell --section "### Console output in different languages"', function (err, stdout, stderr) {
       if (!err) {
         assert.equal(
           stdout,
